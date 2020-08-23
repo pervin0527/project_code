aee8473a48545a85d2aa5aae101ab5ca2de4c3a4
pervin0527 <zfbtldnz@gmail.com>
2020-08-23 19:00:31 +0900

    latest update 2020.08.23

--- source/섹션 2/2. K번째 수/AA.py
+++ source/섹션 2/2. K번째 수/AA.py
@@ -1,6 +1,12 @@
 import sys
 
-sys.stdin = open('C:/Users/user/Desktop/Project_Code/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 2/2. K번째 수/in1.txt', 'r')
+# sys.stdin=open("input.txt", "rt")
 
 T = int(input())
-print(T)
+
+for t in range(T):
+    n, s, e, k = map(int, input().split())
+    a=list(map(int, input().split()))
+    a=a[s-1:e]
+    a.sort()
+    print("#%d %d" %(t+1, a[k-1]))
