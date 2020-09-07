ffc3af413cd8ae52e244da54967ead4d8b6c7668
pervin0527 <zfbtldnz@gmail.com>
2020-09-07 21:42:21 +0900

    latest update 2020.09.07

--- source/섹션 2/9. 주사위 게임/AA.py
+++ source/섹션 2/9. 주사위 게임/AA.py
@@ -1,64 +1,23 @@
-# 1에서부터 6까지의 눈을 가진 3개의 주사위를 던져서 다음과 같은 규칙에 따라 상금을 받는 게임이 있다.
-# 규칙 1. 같은 눈이 3개가 나오면 10,000원 + (같은 눈) * 1,000원의 상금을 받게 된다.
-# 규칙 2. 같은 눈이 2개만 나오는 경우에는 1,000원 + (같은 눈) * 100원의 상금을 받게 된다.
-# 규칙 3. 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈) * 100원의 상금을 받게 된다.
-
-# 예를 들어, 3개의 눈 3, 3, 6이 주어지면 상금은 1,000 + 3 * 100으로 계산되어 1,300원을 받게 된다.
-# 또 3개의 눈이 2,2,2로 주어지면 10,000 + 2* 1,000으로 계산되어 12,000원을 받게 된다.
-# 3개의 눈이 6, 2, 5로 주어지면 그 중 가장 큰 값이 6이므로 6 * 100으로 계산되어 600원을 상금으로 받게 된다.
-# N명이 주사위 게임에 참여하였을 때, 가장 많은 상금을 받은 사람의 상금을 출력하는 프로그램을 작성하시오.
-
-# 입력 설명
-# 첫째 줄에는 참여하는 사람 수 N(2<=N<=1,000)이 주어지고 그 다음 줄부터 N개의 줄에 사람들이 주사위를 던진 3개의 눈이 빈칸을 사이에 두고 각각 주어진다.
-
-# 출력 설명
-# 첫째 줄에 가장 많은 상금을 받은 사람의 상금을 출력한다.
-
-# 입력 예제
-# 3
-# 3 3 6
-# 2 2 2
-# 6 2 5
-
-# 출력 예제
-# 12000
-
 import sys
-
-# sys.stdin = open('C:/Users/user/project_code/source/섹션 2/9. 주사위 게임/in1.txt')
-
-N = int(input())
-
-max = 0
-for i in range(N):
-    a, b, c = map(int, input().split())
-
-    if a == b and a == c:
-        result = 10000 + (a * 1000)
-
-    elif a == b and a != c:
-        result = 1000 + (a * 100)
-
-    elif a != b and a == c:
-        result = 1000 + (a * 100)
-
-    elif b == c and a != b:
-        result = 1000 + (b * 100)
-
+# sys.stdin=open("input.txt", "r")
+max=0
+res=0
+n=int(input())
+for i in range(n):
+    tmp=input().split() 
+    tmp.sort() 
+    a, b, c=map(int, tmp)
+    if a==b and b==c:
+        money=10000+(a*1000);
+    elif a==b or a==c:
+        money=1000+(a*100)
+    elif b==c:
+        money=1000+(b*100)
     else:
-        tmp = 0
-        if a > tmp:
-            tmp = a
+        money=c*100
+    if money > res:
+        res=money
 
-        if b > tmp:
-            tmp = b
+print(res)
 
-        if c > tmp:
-            tmp = c
 
-        result = tmp * 100
-
-    if result > max :
-        max = result
-
-print(max)
