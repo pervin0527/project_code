54d9e71a6be7e9b014786eabb2a262a7988a7b84
pervin0527 <zfbtldnz@gmail.com>
2020-09-17 22:24:13 +0900

    latest update 2020.09.17

---
+++ source/섹션 3/5. 수들의 합/AA.py
@@ -0,0 +1,31 @@
+import sys
+# sys.stdin = open('C:/Users/user/project_code/source/섹션 3/5. 수들의 합/input.txt','r')
+
+N, M = map(int, input().split())
+list_N = list(map(int, input().split()))
+
+lt = 0
+rt = 1
+tot = list_N[0]
+cnt = 0
+
+while True:
+    if tot < M:
+        if rt < N :
+            tot += list_N[rt]
+            rt += 1
+
+        else:
+            break
+
+    elif tot == M:
+        cnt += 1
+        tot -= list_N[lt]
+        lt += 1
+
+    else:
+        tot -= list_N[lt]
+        lt+=1
+
+
+print(cnt)
