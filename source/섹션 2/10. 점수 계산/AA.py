4945e13697f1e1f9e798c408a44a15c91dba03f1
pervin0527 <zfbtldnz@gmail.com>
2020-10-10 21:00:49 +0900

    latest update 2020.10.10

--- source/섹션 2/10. 점수 계산/AA.py
+++ source/섹션 2/10. 점수 계산/AA.py
@@ -1,19 +1,18 @@
 import sys
-
-# sys.stdin = open('C:/Users/user/project_code/source/섹션 2/10. 점수 계산/input.txt')
+# sys.stdin = open('C:/Users/user/project_code/source/섹션 2/10. 점수 계산/input.txt', 'r')
 
 N = int(input())
 list_N = list(map(int, input().split()))
+# print(N, list_N)
 
-sum = 0
-cnt = 0
+cnt = score = 0
 
-for x in list_N:
-    if x == 1:
-        cnt+= 1
-        sum+=cnt
+for i in list_N:
+    if i == 1:
+        cnt += 1
+        score += cnt
 
     else:
-        cnt=0
+        cnt = 0
 
-print(sum)
+print(score)
