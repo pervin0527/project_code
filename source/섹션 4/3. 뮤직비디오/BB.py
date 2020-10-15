0e00f1c3c3840719ae4765146cd67b3687c12325
pervin0527 <zfbtldnz@gmail.com>
2020-10-15 22:58:07 +0900

    latest update 2020.10.15

--- source/섹션 4/3. 뮤직비디오/BB.py
+++ source/섹션 4/3. 뮤직비디오/BB.py
@@ -2,28 +2,41 @@ import sys
 
 sys.stdin = open('C:\\Users\\user\\project_code\\source\\섹션 4\\3. 뮤직비디오\\input.txt', 'r')
 
-# 총 N개의 곡이 라이브 순서 그대로 DVD에 들어간다.
+# N개의 곡이 라이브 순서 그대로 유지되서 DVD에 녹화된다.
 # 한 노래를 쪼개서 두 개의 DVD에 녹화하면 안된다.
-# M개의 DVD에 모든 동영상을 녹화하려 하는데 DVD의 크기를 최소로 하며,
-# M개의 DVD는 모두 같은 크기여야 한다.
+# M개의 DVD에 모든 영상을 녹화하는데 DVD의 크기를 최소로하면서 M개 모두 같은 크기로 만들려고 한다.
+
 N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
 
-# 라이브에서 부른 순서대로 부른 곡의 길이가 분 단위로 주어진다.
-list_N = list(map(int, input().split()))
-# print(N, M, list_N)
+# print(N, M, arr)
+# DVD의 크기를 최소로한다. M개의 DVD 모두 같은 크기로 하려고 한다.
+lt = 1
+rt = sum(arr)
+largest = arr[N-1]
+res = 0
 
-# DVD의 최소 용량 크기를 구하시오.
-start = 1
-end = sum(list_N)
-
-while start <= end:
-    mid = (start + end) // 2
-    
-    cnt = total = 0
-    for x in list_N:
-        if total + x > mid:
+def count(middle):
+    cnt = 1
+    total = 0
+    for x in arr:
+        if total + x > middle: # 조건!!!
             cnt += 1
             total = x
-        
+
         else:
             total += x
+
+    return cnt
+
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if count(mid) <= M and mid >= largest:
+        res = mid
+        rt = mid - 1
+
+    else:
+        lt = mid + 1
+
+print(res)
