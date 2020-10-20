c1caadae69403113ae8400dd53f6df48713580b1
pervin0527 <zfbtldnz@gmail.com>
2020-10-20 21:50:16 +0900

    latest update 2020.10.20

--- source/섹션 4/6. 씨름선수/AA.py
+++ source/섹션 4/6. 씨름선수/AA.py
@@ -1,27 +1,15 @@
 import sys
-# sys.stdin = open('C:\\Users\\user\\project_code\\source\\섹션 4\\6. 씨름선수\\input.txt', 'r')
-
-# 지원자 수 N, N명의 키와 몸무게가 차례로 주어지는데
-n = int(input())
-a = []
-
+# sys.stdin=open("input.txt", "r")
+n=int(input())
+body=[]
 for i in range(n):
-    w, h = map(int, input().split())
-    a.append((w, h))
-
-# 다른 모든 지원자와 일대일 비교하여 키와 몸무게 중 적어도 하나는 크거나 무거운 지원자만 뽑는다.
-a.sort(reverse=True)
-# print(a)
-
-max_height = a[0][0]
-max_width = a[0][1]
-cnt = 1
-
-for i in range(1, n):
-    h, w = a[i][0], a[i][1]
-
-    if max_width < w:
-        cnt += 1
-        max_width = w
-
+    a, b=map(int, input().split())
+    body.append((a, b))
+body.sort(reverse=True)
+largest=0
+cnt=0
+for x, y in body:
+    if y>largest:
+        largest=y
+        cnt+=1
 print(cnt)
