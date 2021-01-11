6c0b85c78851f7dea3f9457d04d410a2b7d28d00
pervin0527 <zfbtldnz@gmail.com>
2021-01-11 21:34:51 +0900

    updated 2021.01.11

--- source/섹션 5/1. 가장 큰 수/AA.py
+++ source/섹션 5/1. 가장 큰 수/AA.py
@@ -1,14 +1,19 @@
 import sys
-# sys.stdin=open("input.txt", "rt")
-num, m=map(int, input().split())
-num=list(map(int, str(num)))
-stack=[]
+# sys.stdin = open('섹션 5/1. 가장 큰 수/input.txt')
+
+n, m = map(int, input().split())
+num = list(map(int, str(n)))
+# print(num)
+
+stack = []
 for x in num:
-    while stack and m>0 and stack[-1]<x:
+    while stack and m > 0 and stack[-1] < x:
         stack.pop()
-        m-=1
+        m -= 1
     stack.append(x)
-if m!=0:
+
+if m != 0:
     stack=stack[:-m]
+
 res=''.join(map(str, stack))
 print(res)
