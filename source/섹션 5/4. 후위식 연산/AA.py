a34950bc8733c6ea56a4fa45f2c2295b32199b9a
pervin0527 <zfbtldnz@gmail.com>
2021-01-17 20:50:57 +0900

    updated 2021.01.17

--- source/섹션 5/4. 후위식 연산/AA.py
+++ source/섹션 5/4. 후위식 연산/AA.py
@@ -1,25 +1,35 @@
 import sys
-# sys.stdin=open("input.txt", "r")
-a=input()
-stack=[]
-for x in a:
-    if x.isdecimal():
+# sys.stdin = open('섹션 5/4. 후위식 연산/input.txt')
+
+formula = input()
+# print(formula)
+stack = []
+result = 0
+
+for x in formula:
+    if x.isdigit():
         stack.append(int(x))
+
     else:
-        if x=='+':
-            n1=stack.pop()
-            n2=stack.pop()
-            stack.append(n2+n1)
-        elif x=='-':
-            n1=stack.pop()
-            n2=stack.pop()
-            stack.append(n2-n1)
-        elif x=='*':
-            n1=stack.pop()
-            n2=stack.pop()
-            stack.append(n2*n1)
-        elif x=='/':
-            n1=stack.pop()
-            n2=stack.pop()
-            stack.append(n2/n1)
+        if x == '+':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b + a)
+
+        elif x == '-':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b - a)
+
+        elif x == '*':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b * a)
+        
+        else:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b / a)
+     
+
 print(stack[0])
--- source/섹션 5/5. 공주구하기/AA.py
+++ source/섹션 5/5. 공주구하기/AA.py
@@ -1,14 +1,18 @@
 import sys
 from collections import deque
-# sys.stdin=open("input.txt", "r")
-n, k=map(int, input().split())
-q=list(range(1, n+1))
-dq=deque(q)
+# sys.stdin = open('섹션 5/5. 공주구하기/input.txt')
+
+n, k = map(int, input().split())
+a = list(x for x in range(1, n + 1))
+dq = deque(a)
+
 while dq:
     for _ in range(k-1):
-        cur=dq.popleft()
-        dq.append(cur)
+        tmp = dq.popleft()
+        dq.append(tmp)
+
     dq.popleft()
-    if len(dq)==1:
+
+    if len(dq) == 1:
         print(dq[0])
         dq.popleft()
