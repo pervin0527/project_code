2c4de82a36e5fcde0c79e992c17bbfcb7cc4ecef
pervin0527 <zfbtldnz@gmail.com>
2021-01-24 14:00:04 +0900

    updated 2021.01.24

--- source/섹션 5/11. 최대힙/AA.py
+++ source/섹션 5/11. 최대힙/AA.py
@@ -1,8 +1,7 @@
 import sys
 import heapq as hq
 
-sys.stdin = open("C:\\Users\\user\\project_code\\source\\섹션 5\\10. 최소힙\\input.txt", "r")
-
+# sys.stdin = open('섹션 5/11. 최대힙/input.txt')
 a = []
 
 while True:
@@ -13,10 +12,10 @@ while True:
 
     if x == 0:
         if len(a) == 0:
-            print(-1)
+            print('-1')
 
         else:
-            print(-hq.heappop(a))
+            print(-(hq.heappop(a)))
 
     else:
         hq.heappush(a, -x)
