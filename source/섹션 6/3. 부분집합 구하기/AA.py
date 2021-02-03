c2548a728bec0adb43b364fff80ec0458592382f
pervin0527 <zfbtldnz@gmail.com>
2021-02-03 09:45:35 +0900

    updated 2021.02.02

--- source/섹션 6/3. 부분집합 구하기/AA.py
+++ source/섹션 6/3. 부분집합 구하기/AA.py
@@ -1,23 +1,22 @@
 import sys
+sys.stdin = open('섹션 6/3. 부분집합 구하기/input.txt')
 
-# sys.stdin = open("C:\\Users\\user\\project_code\\source\\섹션 6\\3. 부분집합 구하기\\input.txt", "r")
-
-def DFS(v):
-    if v > n:
-        for i in range(1, n + 1):
+def DFS(x):
+    if x == n + 1:
+        for i in range(n + 1):
             if ch[i] == 1:
-                print(i, end=' ')
+                print(i, end = ' ')
+
         print()
 
     else:
-        ch[v] = 1 # 원소를 부분집합에 포함시키겠다.
-        DFS(v + 1)
+        ch[x] = 1 # 해당 원소를 부분집합으로 사용하겠다.
+        DFS(x + 1)
+        ch[x] = 0
+        DFS(x + 1)
 
-        ch[v] = 0 # 원소를 부분집합에 포함시키지 않겠다.
-        DFS(v + 1)
-        
 
 if __name__ == "__main__":
     n = int(input())
-    ch = [0] * (n+1)
+    ch = [0] * (n + 1)
     DFS(1)
