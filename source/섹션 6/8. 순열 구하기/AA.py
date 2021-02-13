3888ceffd166159a7544dd2e5d0fc1de9010b80e
pervin0527 <zfbtldnz@gmail.com>
2021-02-13 23:28:39 +0900

    updated 2021.02.13

--- source/섹션 6/8. 순열 구하기/AA.py
+++ source/섹션 6/8. 순열 구하기/AA.py
@@ -1,30 +1,27 @@
 import sys
-# sys.stdin = open('C:\\Users\\user\\project_code\\source\\섹션 6\\8. 순열 구하기\\input.txt')
+sys.stdin = open('섹션 6/8. 순열 구하기/input.txt')
 
-def DFS(L):
+def DFS(level):
     global cnt
-    if L == m:
-        for j in range(m):
-            print(res[j], end=' ')
-
+    if level == m:
+        for i in range(level):
+            print(result[i], end=' ')
         print()
         cnt += 1
 
     else:
         for i in range(1, n+1):
-            if ch[i] == 0:
-                ch[i] = 1
-                res[L] = i
-                DFS(L + 1)
-                ch[i] = 0
-
+            if check[i] == 0:
+                check[i] = 1
+                result[level] = i
+                DFS(level+1)
+                check[i] = 0
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
+    check = [0] * (n + 1)
+    result = [0] * n
     cnt = 0
-    res = [0] * n
-    ch = [0] * (n + 2)
-    # print(n, m)
 
     DFS(0)
     print(cnt)
